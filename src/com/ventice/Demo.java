e2946f92042aca770c9dd63c722decfe30db0b63
ajithreddy17 <areddy.tl48@gmail.com>
2020-06-01 14:19:18 -0500

    first commit

---
+++ src/com/ventice/Demo.java
@@ -0,0 +1,15 @@
+package com.ventice;
+import org.openqa.selenium.WebDriver;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\abusi\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+	}
+
+}
